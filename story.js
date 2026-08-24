c0195fb11b0e955f63a710f78ec0cba746c4978a
johnbrown-IF <fictionman@disroot.org>
2026-08-24 18:51:30 +0300

    Add files via upload

---
+++ story.js
@@ -0,0 +1,34 @@
+var game = {
+	title: 'Название игры',
+	author: 'Ваше имя',
+	version: '1.0',
+	info: 'Игровое вступление',
+	tests: false,
+}
+
+var player = {
+	nam: ['вы', 'себя', 'себе', 'себя', 'собой', 'себе'],
+	desc: 'Вы выглядите как обычно.',
+	loc: 'firstRoom',
+	hidden: true
+}
+
+var globalVerbs = {
+}
+
+var events = {
+	onIntro: function() {
+	},
+	beforeAll: function() {
+	},
+	afterAll: function() {
+	}
+}
+
+var firstRoom = {
+	spec: 'room',
+	desc: 'Тестовая локация'
+}
+
+
+
